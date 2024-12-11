95baa633ab9141fc086e50c06ac66cdf177d179e
Joseph <joseph.galvin@digisolve.co.uk>
2024-12-11 16:32:25 +0000

    map test 2

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,20 @@ import 'map_screen.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-        appBar: AppBar(
-            title: const Center(child: Text("This Is My First App")),
-            backgroundColor: Colors.grey[300]), //AppBar
-        body: const MyApp()),
+      appBar: AppBar(
+        title: Center(
+          child: Image.asset(
+            'images/nash-top-logo.png',
+            width: 100, // Set the width
+            height: 50, // Set the height
+            fit: BoxFit
+                .contain, // Optional: Adjust how the image fits within the dimensions
+          ),
+        ),
+        backgroundColor: const Color.fromARGB(255, 66, 66, 66),
+      ), // AppBar
+      body: const MyApp(),
+    ),
   ));
 }
 
