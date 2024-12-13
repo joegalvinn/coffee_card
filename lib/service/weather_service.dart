79dd072559e54a3f3b7b292d2e60de166b4eed99
Joseph <joseph.galvin@digisolve.co.uk>
2024-12-13 15:23:15 +0000

    test

--- lib/service/weather_service.dart
+++ lib/service/weather_service.dart
@@ -1,21 +1,24 @@
 import 'dart:convert';
-
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import '../models/weather_model.dart';
 
 class WeatherService {
-  // ignore: constant_identifier_names
+  // Base URL for OpenWeatherMap API
   static const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
   final String apiKey;
 
+  // Constructor for WeatherService, requires an API key
   WeatherService(this.apiKey);
 
+  // Function to fetch weather information based on the city name
   Future<Weather> getWeather(String cityName) async {
+    // Prepend CORS proxy URL to avoid CORS issue when using the browser
+    const String corsProxy = 'https://cors-anywhere.herokuapp.com/';
+
     final response = await http.get(
-      Uri.parse(
-          '$BASE_URL?q=$cityName&appid=$apiKey&units=metric'), // Corrected "metric"
+      Uri.parse('$corsProxy$BASE_URL?q=$cityName&appid=$apiKey&units=metric'),
     );
 
     if (response.statusCode == 200) {
@@ -25,6 +28,7 @@ class WeatherService {
     }
   }
 
+  // Function to get the current city based on the device's GPS coordinates
   Future<String> getCurrentCity() async {
     // Check location permissions
     LocationPermission permission = await Geolocator.checkPermission();
@@ -38,14 +42,14 @@ class WeatherService {
       throw Exception('Location permissions are permanently denied');
     }
 
-    // Fetch the current position
+    // Fetch the current position (GPS coordinates)
     Position position = await Geolocator.getCurrentPosition(
       locationSettings: const LocationSettings(
         accuracy: LocationAccuracy.high, // High accuracy
       ),
     );
 
-    // Convert the location into a list of placemark objects
+    // Convert the location into a list of placemark objects (city info)
     List<Placemark> placemarks =
         await placemarkFromCoordinates(position.latitude, position.longitude);
 
